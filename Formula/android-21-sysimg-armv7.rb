bc686e493046ab19405bcf830de86d6d7435bcca
Nathan Toone <nathan@toonetown.com>
2015-04-14 14:40:53 -0600

    Update to latest releases

--- Formula/android-21-sysimg-armv7.rb
+++ Formula/android-21-sysimg-armv7.rb
@@ -2,9 +2,9 @@ require 'pathname'
 require Pathname(__FILE__).realpath.dirname.join('../lib', 'android-sdk-formula')
 
 class Android21SysimgArmv7 < AndroidSysimgFormula
-  url 'http://dl-ssl.google.com/android/repository/sys-img/android/sysimg_arm-21_r01.zip'
-  version '1'
-  sha1 '74a74b89a06ffba03448ccdfbb414c49026e660c'
+  url 'http://dl-ssl.google.com/android/repository/sys-img/android/sysimg_arm-21_r03.zip'
+  version '3'
+  sha1 '0b2e21421d29f48211b5289ca4addfa7f4c7ae5a'
   api_version '21'
   abi 'armeabi-v7a'
 end
