9ed8c941ffa7caa953eff50a41f9d249d0f3b957
Nathan Toone <nathan@toonetown.com>
2015-10-26 12:29:54 -0600

    Update to the latest versions

--- Formula/android-23-sysimg-i386.rb
+++ Formula/android-23-sysimg-i386.rb
@@ -2,9 +2,9 @@ require 'pathname'
 require Pathname(__FILE__).realpath.dirname.join('../lib', 'android-sdk-formula')
 
 class Android23SysimgI386 < AndroidSysimgFormula
-  url 'http://dl-ssl.google.com/android/repository/sys-img/android/sysimg_x86-23_r03.zip'
-  version '3'
-  sha1 '3cb2e8efb575c35a558b091eac7e1bc5843f5f12'
+  url 'http://dl-ssl.google.com/android/repository/sys-img/android/sysimg_x86-23_r04.zip'
+  version '4'
+  sha1 'abd300ad36bae688ce78aadcf83e2d8846f8bce8'
   api_version '23'
   abi 'x86'
 end
--- Formula/android-23-sysimg-x86_64.rb
+++ Formula/android-23-sysimg-x86_64.rb
@@ -2,9 +2,9 @@ require 'pathname'
 require Pathname(__FILE__).realpath.dirname.join('../lib', 'android-sdk-formula')
 
 class Android23SysimgX8664 < AndroidSysimgFormula
-  url 'http://dl-ssl.google.com/android/repository/sys-img/android/sysimg_x86_64-23_r03.zip'
-  version '3'
-  sha1 '226510856431bc4a73690540a8d7cbad974bedd3'
+  url 'http://dl-ssl.google.com/android/repository/sys-img/android/sysimg_x86_64-23_r04.zip'
+  version '4'
+  sha1 '07ba4d1c9bd0cbca28119f3bd1387c5f05e3d7c0'
   api_version '23'
   abi 'x86_64'
 end
