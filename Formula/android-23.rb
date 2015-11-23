68baf1bcd6aeaccfbeac99154321d445be017b5d
Nathan Toone <nathan@toonetown.com>
2015-11-23 10:46:46 -0700

    Update to the latest versions

--- Formula/android-23.rb
+++ Formula/android-23.rb
@@ -2,9 +2,9 @@ require 'pathname'
 require Pathname(__FILE__).realpath.dirname.join('../lib', 'android-sdk-formula')
 
 class Android23 < AndroidPlatformFormula
-  url 'http://dl-ssl.google.com/android/repository/android-23_r01.zip'
-  version '1'
-  sha1 'cbccca8d3127e894845556ce999b28281de541bd'
+  url 'http://dl-ssl.google.com/android/repository/android-23_r02.zip'
+  version '2'
+  sha1 'a71706896fe8159015c1c02f63af6ea03ceeb79e'
   api_version '23'
 
   # Dependencies for docs, sources, and samples
