fadb2d51bc6eecb380595ca966ea224e498d1e26
Nathan Toone <nathan@toonetown.com>
2016-05-18 20:28:01 -0600

    Update to the latest versions

--- Formula/google-apis-22-sysimg-i386.rb
+++ Formula/google-apis-22-sysimg-i386.rb
@@ -3,9 +3,9 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "android-sdk-formula"
 
 class GoogleApis22SysimgI386 < AndroidSysimgFormula
   desc "Google APIs Intel x86 Atom System Image"
-  url "http://dl.google.com/android/repository/sys-img/google_apis/sysimg_x86-22_r06.zip"
-  version "6"
-  sha1 "66f527269d35f3132da99807e5ce1e5585d30df2"
+  url "http://dl.google.com/android/repository/sys-img/google_apis/sysimg_x86-22_r07.zip"
+  version "7"
+  sha1 "5752e307c6649425a33f8393acdfccca71a9a997"
   api_version "22"
   abi "x86"
   sysimg_tag "/google_apis"
