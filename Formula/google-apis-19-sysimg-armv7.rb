bb7a38ae92963529b322e078f031c6d22f666493
Nathan Toone <nathan@toonetown.com>
2016-05-19 18:04:26 -0600

    Update to the latest versions

--- Formula/google-apis-19-sysimg-armv7.rb
+++ Formula/google-apis-19-sysimg-armv7.rb
@@ -3,9 +3,9 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "android-sdk-formula"
 
 class GoogleApis19SysimgArmv7 < AndroidSysimgFormula
   desc "Google APIs ARM EABI v7a System Image"
-  url "http://dl.google.com/android/repository/sys-img/google_apis/sysimg_armeabi-v7a-19_r20.zip"
-  version "20"
-  sha1 "ba8eff47ea7378fe858c258be7b61ac2a158e65c"
+  url "http://dl.google.com/android/repository/sys-img/google_apis/sysimg_armeabi-v7a-19_r21.zip"
+  version "21"
+  sha1 "a1b45e3b2e30a368f4842185017f5dcbf32e3bd8"
   api_version "19"
   abi "armeabi-v7a"
   sysimg_tag "/google_apis"
--- Formula/google-apis-19.rb
+++ Formula/google-apis-19.rb
@@ -3,9 +3,9 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "android-sdk-formula"
 
 class GoogleApis19 < AndroidPlatformFormula
   desc "Android + Google APIs"
-  url "http://dl.google.com/android/repository/google_apis-19_r18.zip"
-  version "18"
-  sha1 "701595b0d9985cca00be29b3c606e64731c3e1f7"
+  url "http://dl.google.com/android/repository/google_apis-19_r20.zip"
+  version "20"
+  sha1 "5b933abe830b2f25b4c0f171d45e9e0651e56311"
   api_version "19"
   target_directory "add-ons"
   target_name      "addon-google_apis-google"
