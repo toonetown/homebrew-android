1569c21bd7c7e1b2df06982ab7bb1d98a59c318e
Nathan Toone <nathan@toonetown.com>
2016-05-27 11:08:13 -0600

    Update to the latest versions

--- Formula/google-m2repository.rb
+++ Formula/google-m2repository.rb
@@ -5,9 +5,9 @@ require Pathname(__FILE__).realpath.dirname.join("../lib", "android-tool-formula
 
 class GoogleM2repository < AndroidToolFormula
   desc "Local Maven repository for Support Libraries"
-  url "https://dl.google.com/android/repository/google_m2repository_r26.zip"
-  version "26"
-  sha1 "fb4953a28b726a4428aeb150dff9985a7d7abae0"
+  url "https://dl.google.com/android/repository/google_m2repository_r27.zip"
+  version "27"
+  sha1 "4464fc20784225fc190d172211c1b5cef946e911"
   depends_on "toonetown/android/android-sdk"
 
   def install
