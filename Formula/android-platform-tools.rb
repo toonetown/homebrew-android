962e79f8458327a8affb2c39ca9a53bf4c1903c3
Nathan Toone <nathan@toonetown.com>
2016-10-14 22:05:37 -0600

    Update to the latest versions

--- Formula/android-platform-tools.rb
+++ Formula/android-platform-tools.rb
@@ -4,9 +4,9 @@ require "pathname"
 require Pathname(__FILE__).realpath.dirname.join("../lib", "android-tool-formula") unless defined?(android_tool_formula)
 
 class AndroidPlatformTools < AndroidToolFormula
-  url "https://dl.google.com/android/repository/platform-tools_r24.0.3-macosx.zip"
-  version "24.0.3"
-  sha1 "8210f0c7430e35020f19eae73a0c365d4e839501"
+  url "https://dl.google.com/android/repository/platform-tools_r24.0.4-macosx.zip"
+  version "24.0.4"
+  sha1 "43b8dd039fff5bb3fcc4e32f44feaa5aaff91cf0"
 
   depends_on "toonetown/android/android-sdk"
 
